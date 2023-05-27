77b6ffb8af6c5b2300bf0d4b9965c3bb8129bbe2
Sagar Chowdhury <sagarchowdhury4693@gmail.com>
2023-05-27 09:45:19 +0530

    Added some functions frot the Api

--- BuildingApi/main.go
+++ BuildingApi/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Course struct {
-	CouseId     string  `json:"courseid"`
+	CourseId    string  `json:"courseid"`
 	CourseName  string  `json:"coursename"`
 	CoursePrice int     `json:"price"`
 	Author      *Author `json:"author"`
@@ -15,9 +23,43 @@ type Author struct {
 var courses []Course
 
 func (c *Course) IsEmpty() bool {
-	return c.CouseId == "" && c.CourseName == ""
+	return c.CourseId == "" && c.CourseName == ""
 }
 
 func main() {
+
+	fmt.Println("API - LearnCodeOnline.in")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1> Welcome To the home page of the new API</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json") //By setting the "Content-Type" header to "application/json",
+	//the client receiving the response will know how to interpret the response data.
+	//It informs the client that the response body contains JSON-encoded data.
+	json.NewEncoder(w).Encode(courses)
+
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json") //setiing content type.
+	params := mux.Vars(r)
+	//loop through courses , find matching id and return the response.
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No Course found with given id")
 
 }
